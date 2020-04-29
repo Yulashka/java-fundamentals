d875807bc78e51ffec077c751d1c7cb14f556857
Iuliia Bassovets <iuliia@Iuliias-MacBook-Pro.local>
2020-04-29 10:27:40 -0700

    Adding helloWorld and initials programs

---
+++ COS-126/Initials.java
@@ -0,0 +1,16 @@
+// Initials program
+public class Initials {
+
+    public static void main(String[] args) {
+        System.out.println("**             **********");
+        System.out.println("**                     *");
+        System.out.println("**                    *");
+        System.out.println("**                   *");
+        System.out.println("**                  *");
+        System.out.println("**                 *");
+        System.out.println("**                *");
+        System.out.println("**               *");
+        System.out.println("**              ***********");
+    }
+
+}
