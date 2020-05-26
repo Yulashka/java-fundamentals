890513da8e81e815e400cfb84c9eff24af526120
Iuliia Bassovets <iuliia@lepistina.com>
2020-05-26 16:38:17 -0700

    prints out n in binary

---
+++ Loops_and_Conditionals/src/Binary.java
@@ -0,0 +1,44 @@
+/******************************
+ *  Prints out n in binary.
+ * 5 -> 101
+ * 106 -> 1101010
+ * 0 -> 0
+ * 16 -> 10000
+ *  10000
+ *  Does not handle negative integers.
+ *  could use Integer.toBinaryString(N) instead.
+ *************************************/
+
+public class Binary {
+    public static void main(String[] args) {
+
+        // read in the command-line argument
+        int n = Integer.parseInt(args[0]);
+
+        // set power to the largest power of 2 that is <= n
+        int power = 1;
+        while (power <= n/2) {
+            power *= 2;
+        }
+
+        // check for presence of powers of 2 in n, from largest to smallest
+        while (power > 0) {
+
+            // power is not present in n
+            if (n < power) {
+                System.out.print(0);
+            }
+
+            // power is present in n, so subtract power from n
+            else {
+                System.out.print(1);
+                n -= power;
+            }
+
+            // next smallest power of 2
+            power /= 2;
+        }
+        System.out.println();
+
+    }
+}
